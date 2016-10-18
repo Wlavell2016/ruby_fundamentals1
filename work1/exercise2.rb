8ffabf5fb93d337d519132923107b92fb3c5aed9
wlavell <william.lavell@gmail.com>
2016-10-18 12:59:17 -0500

    starting exercise 3

--- work1/exercise2.rb
+++ work1/exercise2.rb
@@ -1,1 +1,28 @@
-exercise2.rb
+# This is to make a tip total
+total = 55
+tip = 0.15
+
+tip_amount = total * tip
+finalamount = tip_amount + total
+
+puts finalamount
+
+# This is exercise number 2
+number = 10
+string1 = " where is "
+string2 = " street?"
+
+puts string1 + number.to_s + string2
+
+# This is for exercise number 3
+
+num1 = 45628
+num2 = 7839
+
+output = num1 * num2
+puts output.to_s
+
+
+# execrise number 4
+a =(10 < 20 && 30 < 20) || !(10 == 11)
+puts a
